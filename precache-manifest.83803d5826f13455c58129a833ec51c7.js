62df6c6bee2683fbf8eceb218753f81f03efc7d7
Jon Long <jonbrantleylong@gmail.com>
2020-03-19 12:11:30 -0400

    Updates

---
+++ precache-manifest.83803d5826f13455c58129a833ec51c7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d0dea5d973ecbdc3033e5894ee9b114",
+    "url": "/ui-exercise/index.html"
+  },
+  {
+    "revision": "77d2c3f8f8be140c3d3f",
+    "url": "/ui-exercise/static/css/main.b602039e.chunk.css"
+  },
+  {
+    "revision": "501d143dbf5d05ebfa97",
+    "url": "/ui-exercise/static/js/2.c07f0c87.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ui-exercise/static/js/2.c07f0c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d2c3f8f8be140c3d3f",
+    "url": "/ui-exercise/static/js/main.e1e85851.chunk.js"
+  },
+  {
+    "revision": "eb124c59583a3da59322",
+    "url": "/ui-exercise/static/js/runtime-main.e1400b1d.js"
+  },
+  {
+    "revision": "0c9c3f25e88c10d02bfa540a09e549df",
+    "url": "/ui-exercise/static/media/back.0c9c3f25.svg"
+  },
+  {
+    "revision": "defa845652d9bacfabe4273858cb89a4",
+    "url": "/ui-exercise/static/media/delete.defa8456.svg"
+  },
+  {
+    "revision": "807fd0755d4933958218eb8d77aadffe",
+    "url": "/ui-exercise/static/media/forward.807fd075.svg"
+  },
+  {
+    "revision": "c990e201f21b9b0fa87f2a886cc9f3e8",
+    "url": "/ui-exercise/static/media/hamburger.c990e201.svg"
+  },
+  {
+    "revision": "646d40c76fc6489ee833910b88a2a30d",
+    "url": "/ui-exercise/static/media/read.646d40c7.svg"
+  },
+  {
+    "revision": "a8c22a058e4e76536c8d038f935a27a7",
+    "url": "/ui-exercise/static/media/reply.a8c22a05.svg"
+  },
+  {
+    "revision": "a4e6bfc7a854c6ebc18a33b694e9dc37",
+    "url": "/ui-exercise/static/media/search.a4e6bfc7.svg"
+  },
+  {
+    "revision": "37857ad3f846f65476cffb20d3dfc4e5",
+    "url": "/ui-exercise/static/media/star.37857ad3.svg"
+  },
+  {
+    "revision": "2747b472fc360984abd229caf5ba3a81",
+    "url": "/ui-exercise/static/media/unread.2747b472.svg"
+  }
+]);
